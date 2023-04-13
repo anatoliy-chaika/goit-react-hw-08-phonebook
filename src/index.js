da4abb0aaa1cc24d047e8aab6912944a5b14a2c5
anatoliy-chaika <110462151+anatoliy-chaika@users.noreply.github.com>
2023-04-13 21:56:12 +0300

    start

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import { store } from 'components/redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
